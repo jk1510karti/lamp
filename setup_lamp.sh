58bd7392928f170b72be2960f6c1bb284182bb45
Karthik Jaganathan <13429665+jk1510karti@users.noreply.github.com>
2020-05-17 19:41:18 +0100

    create lamp-setup shell script

---
+++ setup_lamp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#*****************************************************#
+# This script is developed only for the test purposes #
+#*****************************************************#
+# Script to install LAMP stack on CentOS8
+# The database either can be MariaDB/MySQL.
+# Update the yum repo
+sudo yum repo
+
+# Install apache, php and mariaDB
+sudo yum install httpd mariadb-server mariadb php php-mysqlnd -y
+
+# Start all the services
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable mariadb
+sudo systemctl start mariadb # Root user password reset setup is not done yet
+
+# Restart the appserver
+sudo systemctl restart httpd
+
+# Enable firewal rule for both http and https. Just in case if we have to enable SSL later
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+
+# Create test webpage
+sudo echo "Hello World" > /var/www/html/index.html
